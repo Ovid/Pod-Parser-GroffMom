a43a41ec55a0632b8d9947fdff29239b8ac5f13e
Curtis Poe <ovid@cpan.org>
2009-10-25 12:13:50 +0000

    Lots of subtle tweaks to get my Prolog article formatted correctly.

--- lib/Pod/Parser/Groffmom.pm
+++ lib/Pod/Parser/Groffmom.pm
@@ -2,20 +2,39 @@ package Pod::Parser::Groffmom;
 
 use warnings;
 use strict;
-use Carp 'croak';
+use Carp qw(carp croak);
+use Perl6::Junction 'any';
 
 use Moose;
-extends 'Pod::Parser';
+use Moose::Util::TypeConstraints 'enum';
+extends qw(Pod::Parser Moose::Object);
 
-my @MOM_METHODS = qw(title subtitle author copyright cover);
+# order of MOM_METHODS is very important.  See '.COPYRIGHT' in mom docs.
+my @MOM_METHODS  = qw(title subtitle author copyright);
+my @MOM_BOOLEANS = qw(cover newpage);
+my %IS_MOM       = map { uc($_) => 1 } ( @MOM_METHODS, @MOM_BOOLEANS );
 
-foreach my $method (@MOM_METHODS) {
+foreach my $method ( @MOM_METHODS, @MOM_BOOLEANS ) {
     has $method       => ( is => 'rw', isa => 'Bool' );
     has "mom_$method" => ( is => 'rw', isa => 'Str' );
 }
-has head     => ( is => 'rw' );
-has subhead  => ( is => 'rw' );
-has mom_text => ( is => 'rw', isa => 'Str', default => '' );
+has head    => ( is => 'rw' );
+has subhead => ( is => 'rw' );
+has mom     => ( is => 'rw', isa => 'Str', default => '' );
+
+# list helpers
+has list_data => ( is => 'rw', isa => 'Str', default => '' );
+has in_list_mode => ( is => 'rw', isa => 'Bool' );
+has list_type => ( is => 'rw', isa => enum( [qw/BULLET DIGIT/] ) );
+
+sub mom_methods  { @MOM_METHODS }
+sub mom_booleans { @MOM_BOOLEANS }
+
+sub is_mom {
+    my ( $class, $command ) = @_;
+    return 1 if $command eq 'NAME'; # special alias for 'TITLE'
+    return $IS_MOM{$command};
+}
 
 =head1 NAME
 
@@ -44,50 +63,106 @@ sub _escape {
 
 sub command {
     my ( $self, $command, $paragraph, $line_num ) = @_;
-    $self->$_(0) foreach @MOM_METHODS;
+
+    # reset mom_methods
+    $self->$_(0) foreach $self->mom_methods;
     $paragraph = $self->_trim($paragraph);
 
-    # =head1 TITLE (upper case)
-    my $is_mom = $paragraph eq uc $paragraph;
+    my $is_mom = $self->is_mom($paragraph);
     $paragraph = lc $paragraph if $is_mom;
     print "Command $command ($paragraph)\n";
     if ($is_mom) {
-        $paragraph = 'title' if $paragraph eq 'name';
-        foreach my $method (@MOM_METHODS) {
-            my $mom_method = "mom_$method";
-            if ( $paragraph eq $method ) {
-                if ( my $item = $self->$mom_method ) {
-                    croak("Tried to reset $method ($item) at line $line_num");
-                }
-                $self->$method(1);
-                $self->mom_cover(1) if $paragraph eq 'cover';
-                return;
-            }
-        }
+        $self->parse_mom( $command, $paragraph, $line_num );
     }
     else {
-        $paragraph = $self->_escape($paragraph);
-        if ( 'head1' eq $command ) {
-            $self->add_to_mom(qq{.HEAD "$paragraph"\n});
+        $self->build_mom( $command, $paragraph, $line_num );
+    }
+}
+
+sub parse_mom {
+    my ( $self, $command, $paragraph, $line_num ) = @_;
+    $paragraph = 'title' if $paragraph eq 'name';
+    foreach my $method ( $self->mom_methods ) {
+        my $mom_method = "mom_$method";
+        if ( $paragraph eq $method ) {
+            if ( my $item = $self->$mom_method ) {
+                croak("Tried to reset $method ($item) at line $line_num");
+            }
+            $self->$method(1);
+            return;
         }
-        if ( 'head2' eq $command ) {
-            $self->add_to_mom(qq{.SUBHEAD "$paragraph"\n});
+    }
+    $self->mom_cover(1)             if $paragraph eq 'cover';
+    $self->add_to_mom(".NEWPAGE\n") if $paragraph eq 'newpage';
+}
+
+sub build_mom {
+    my ( $self, $command, $paragraph, $line_num ) = @_;
+    $paragraph = $self->_escape($paragraph);
+    if ( 'head1' eq $command ) {
+        $self->add_to_mom(qq{.HEAD "$paragraph"\n\n});
+    }
+    elsif ( 'head2' eq $command ) {
+        $self->add_to_mom(qq{.SUBHEAD "$paragraph"\n\n});
+    }
+    elsif ( 'head3' eq $command ) {
+        $self->add_to_mom(qq{\\f[B]$paragraph\\f[P]\n\n});
+    }
+    elsif ( any(qw/over item back/) eq $command ) {
+        $self->build_list( $command, $paragraph, $line_num );
+    }
+    else {
+        carp("Unknown command ($command) at line $line_num");
+    }
+}
+
+sub build_list {
+    my ( $self, $command, $paragraph, $line_num ) = @_;
+    if ( 'over' eq $command ) {
+        $self->in_list_mode(1);
+    }
+    elsif ( 'back' eq $command ) {
+        my $list =
+            ".L_MARGIN 1.25i\n.LIST "
+          . $self->list_type . "\n"
+          . $self->list_data
+          . ".LIST END\n\n.L_MARGIN 1i\n";
+        $self->add_to_mom($list);
+        $self->list_data('');
+        $self->in_list_mode(0);
+    }
+    elsif ( 'item' eq $command ) {
+        if ( not $self->in_list_mode ) {
+            carp("Found (=item $paragraph) outside of list at line $line_num.  Discarding");
+            return;
         }
+        $paragraph =~ /^(\*|\d+)?\s*(.*)/;
+        my ( $list_type, $item ) = ( $1, $2 );
+
+        # default to BULLET if we cannot identify the list type
+        $self->list_type( $list_type ne '*' ? 'DIGIT' : 'BULLET' )
+          if not $self->list_type;
+        $self->add_to_list(".ITEM\n$item\n");
     }
 }
 
 sub add_to_mom {
     my ( $self, $text ) = @_;
     return unless defined $text;
-    $self->mom_text( ($self->mom_text||'') . $text );
+    $self->mom( ( $self->mom || '' ) . $text );
+}
+
+sub add_to_list {
+    my ( $self, $text ) = @_;
+    return unless defined $text;
+    $self->list_data( ( $self->list_data || '' ) . $text );
 }
 
 sub end_input {
     my $self = shift;
     my $mom  = '';
 
-    foreach my $method (@MOM_METHODS) {
-        next if 'cover' eq $method;
+    foreach my $method ( $self->mom_methods ) {
         my $mom_method = "mom_$method";
         my $macro      = ".\U$method";
         if ( my $item = $self->$mom_method ) {
@@ -98,9 +173,8 @@ sub end_input {
     }
     if ( $self->mom_cover ) {
         my $cover = ".COVER";
-        foreach my $method (@MOM_METHODS) {
+        foreach my $method ( $self->mom_methods ) {
             my $mom_method = "mom_$method";
-            next if 'cover' eq $method;
             next unless $self->$mom_method;
             $cover .= " \U$method";
         }
@@ -110,31 +184,28 @@ sub end_input {
 .PRINTSTYLE TYPESET
 \#
 .FAM H
-.PT 12
+.PT_SIZE 12
 .START
 END
-    $mom .= $self->mom_text;
-    open my $fh, '>', 'out.mom' or die "$!";
-    print $fh $mom;
-    print $mom;
+    $self->mom($mom .= $self->mom);
 }
-
-sub _head1 { }
-sub _head2 { }
 
 sub verbatim {
     my ( $self, $verbatim, $paragraph, $line_num ) = @_;
     $paragraph = $self->_trim($paragraph);
     print "Verbatim ($verbatim) ($paragraph)\n";
-    $self->add_to_mom(sprintf <<'END' => $verbatim);
+    $verbatim =~ s/\s+$//s;
+    $self->add( sprintf <<'END' => $verbatim );
 .FAM C
+.PT_SIZE 10
 .LEFT
 .L_MARGIN 1.25i
 %s
 .QUAD
 .L_MARGIN 1i
 .FAM H
-.PT 12
+.PT_SIZE 12
+
 END
 }
 
@@ -142,7 +213,8 @@ sub textblock {
     my ( $self, $textblock, $paragraph, $line_num ) = @_;
     $textblock = $self->_escape( $self->_trim($textblock) );
     print "Textblock $textblock ($paragraph)\n";
-    foreach my $method (@MOM_METHODS) {
+    $textblock = $self->interpolate($textblock, $line_num);
+    foreach my $method ( $self->mom_methods ) {
         my $mom_method = "mom_$method";
         if ( $self->$method ) {
 
@@ -151,18 +223,32 @@ sub textblock {
             return;
         }
     }
-    $self->add_to_mom($textblock);
+    $self->add("$textblock\n\n");
+}
+
+sub add {
+    my ( $self, $data ) = @_;
+    my $add = $self->in_list_mode ? 'add_to_list' : 'add_to_mom';
+    $self->$add($data);
 }
 
 sub interior_sequence {
     my ( $self, $sequence, $paragraph, $line_num ) = @_;
     $paragraph = $self->_trim($paragraph);
-    print "Sequence $sequence ($paragraph)\n";
+    if ( $sequence eq 'I' ) {
+        return "\\f[I]$paragraph\\f[P]";
+    }
+    elsif ( $sequence eq 'C' ) {
+        return "\\f[C]$paragraph\\f[P]";
+    }
+    elsif ( $sequence eq 'B' ) {
+        return "\\f[B]$paragraph\\f[P]";
+    }
+    else {
+        carp("Uknown sequence ($sequence<$paragraph> at line $line_num)");
+        return "$sequence<$paragraph>";
+    }
 }
-
-# verbatim()
-# textblock()
-# interior_sequence()
 
 =head1 SYNOPSIS
 
